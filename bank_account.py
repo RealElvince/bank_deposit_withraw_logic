3507e8cf48a433adf338471ab2f371c4116432a6
RealElvince <obuongely@yahoo.com>
2025-11-01 16:42:09 +0300

    list for transactions.

--- bank_account.py
+++ bank_account.py
@@ -2,6 +2,7 @@ class BankAccount:
     def __init__(self,account_holder,account_balance):
         self.account_holder = account_holder
         self.account_balance = account_balance
+        self.transactions = []
 
     
     def deposit(self,amount):
