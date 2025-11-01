315d0c2169cfe44112709bf2cb27a855eef17093
RealElvince <obuongely@yahoo.com>
2025-11-01 16:24:25 +0300

    error handling insufficient funds.

--- bank_account.py
+++ bank_account.py
@@ -16,3 +16,6 @@ class BankAccount:
         elif self.account_balance >= amount:
             self.account_balance -= amount
             print(f"Dear {self.account_holder}, you have withdrawn {amount}. Your new balance is {self.account_balance}.")
+
+        else:
+            print("Insufficient Funds!")
