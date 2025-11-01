1e5985f08fa885112c16d7f1d7ed3e8b1cd55b8d
RealElvince <obuongely@yahoo.com>
2025-11-01 16:11:15 +0300

    created BanKAccount class

---
+++ bank_account.py
@@ -0,0 +1,7 @@
+class BankAccount:
+    def __init__(self,account_holder,account_balance):
+        self.account_holder = account_holder
+        self.account_balance = account_balance
+
+    
+    
