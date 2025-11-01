61a00f8ceff5c6189e63040541b3b7d12fc9e8c8
RealElvince <obuongely@yahoo.com>
2025-11-01 16:18:07 +0300

    print successful deposit message.

--- bank_account.py
+++ bank_account.py
@@ -7,3 +7,4 @@ class BankAccount:
     def deposit(self,amount):
         if amount > 0:
             self.account_balance += amount
+            print(f"Dear {self.account_holder},{amount} has been deposited into your bank account. New balance is {self.account_balance}.")
